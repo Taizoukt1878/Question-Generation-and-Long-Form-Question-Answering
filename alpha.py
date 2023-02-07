85b554f9fe8a2cb37ff003a93c40d41ab4623401
Anouar Taizoukt <taizouktanouar@gmail.com>
2023-02-07 18:59:18 +0100

    Second Commit

---
+++ alpha.py
@@ -0,0 +1,164 @@
+#import pandas as pd
+import streamlit as st
+from pipelines import pipeline
+from PyPDF2 import PdfReader
+#haystack
+from haystack.document_stores import PineconeDocumentStore
+from haystack import Document
+from haystack.nodes import Seq2SeqGenerator
+from haystack.nodes import EmbeddingRetriever
+from haystack.pipelines import GenerativeQAPipeline
+
+
+
+st.set_page_config(layout="wide")
+
+
+#Functions
+
+def connect_to_doc_store():
+	document_store = PineconeDocumentStore(
+    api_key='d27dc937-4ba8-45a8-8418-15e47115a033',
+    index='haystack-lfqa',
+    similarity="cosine",
+    embedding_dim=768
+    )
+	return document_store
+
+def get_retriever(document_store):
+
+	retriever = EmbeddingRetriever(
+    document_store=document_store,
+    embedding_model="flax-sentence-embeddings/all_datasets_v3_mpnet-base",
+    model_format="sentence_transformers"
+	)
+	return retriever
+
+def get_generator():
+	generator = Seq2SeqGenerator(model_name_or_path="vblagoje/bart_lfqa")
+	return generator
+
+def get_pipeline(generator, retriever):
+	pipe = GenerativeQAPipeline(generator, retriever)
+	return pipe
+
+def update_doc_store(doc: str, article_title : str = "None", section_title : str = "None" ):
+	docs = []
+	doc = Document(
+        	content= doc,
+        	meta={
+            	"article_title": article_title,
+            	'section_title': section_title
+        	}
+    	)
+	docs.append(doc)
+	embeds = retriever.embed_documents(docs)
+	doc.embedding = embeds[0]
+	document_store.write_documents(docs)
+
+def text_from_pdf_qg(path: str):
+	nlp = pipeline("e2e-qg")
+	reader = PdfReader(path)
+	pages = reader.pages
+	questions = []
+	all_doc = str()
+	for page in pages:
+		doc = page.extract_text()
+	#	all_doc = all_doc + doc + " "
+		quest = nlp(doc)
+		for item in quest:
+			questions.append(item)
+	#update_doc_store(doc)
+	return questions
+
+
+def text_from_pdf(path: str) -> str:
+	reader = PdfReader(path)
+	pages = reader.pages
+	doc = str()
+	for page in pages:
+		doc = doc + page.extract_text() + " "
+	update_doc_store(doc)
+
+def get_answer(question):
+	document_store = connect_to_doc_store()
+	retriever = get_retriever(document_store)
+	generator = get_generator()
+	pipe = get_pipeline(generator, retriever)
+	result = pipe.run(
+	query=str(question),
+	params={
+        "Retriever": {"top_k": 3},
+        "Generator": {"top_k": 1}
+    	}
+	)
+	answer_dic = result["answers"][0].to_dict()
+	return answer_dic
+
+
+
+
+
+
+# Page containers
+
+header = st.container()
+
+section1 = st.container()
+
+section2 = st.container()
+
+section3 = st.container()
+
+section4 = st.container()
+
+
+
+
+
+
+# Header section
+
+with header:
+	st.title("Question Generation, and Long Form Question Answering")
+
+
+with section1:
+
+	#col1, col2 = st.columns(2)
+
+	# Reading pdf files
+	#with col1 :
+	st.title("PDF")
+	file = st.file_uploader("Upload a pdf file")
+		#text_from_pdf(file)
+	# Writing a question
+	#with col2:
+	st.title("input question")
+	question_input = st.text_input("Ask a question")
+
+# Generating questions
+
+with section2:
+	if(file):
+		questions = text_from_pdf_qg(file)
+		st.title("Generated question")
+		question_gen = st.radio("Generated questions", questions)
+		st.write(question_gen)
+
+
+with section3:
+	if st.button("Submit generated question"):
+		st.write("question: ", question_gen)
+		answer_dic = get_answer(question_gen)
+		st.subheader("Answer")
+		st.write(str(answer_dic["answer"]))
+
+with section4:
+		if st.button("Submit question"):
+			st.write("question: ", question_input)
+			answer_dic = get_answer(question_input)
+			st.subheader("Answer")
+			st.write(str(answer_dic["answer"]))
+
+
---
+++ pipelines.py
@@ -0,0 +1,190 @@
+import itertools
+import logging
+from typing import Optional, Dict, Union
+
+from nltk import sent_tokenize
+
+import torch
+from transformers import(
+    AutoModelForSeq2SeqLM, 
+    AutoTokenizer,
+    PreTrainedModel,
+    PreTrainedTokenizer,
+)
+
+logger = logging.getLogger(__name__)
+
+class E2EQGPipeline:
+    def __init__(
+        self,
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizer,
+        use_cuda: bool #Helps making the process considerably faster
+    ) :
+
+        self.model = model
+        self.tokenizer = tokenizer
+
+        self.device = "cuda" if torch.cuda.is_available() and use_cuda else "cpu"
+        self.model.to(self.device)
+
+        assert self.model.__class__.__name__ in ["T5ForConditionalGeneration", "BartForConditionalGeneration"]
+        
+        if "T5ForConditionalGeneration" in self.model.__class__.__name__:
+            self.model_type = "t5"
+        else:
+            self.model_type = "bart"
+        
+        self.default_generate_kwargs = {
+            "max_length": 256,
+            "num_beams": 4,
+            "length_penalty": 1.5,
+            "no_repeat_ngram_size": 3,
+            "early_stopping": True,
+        }
+    
+    def __call__(self, context: str, **generate_kwargs):
+        inputs = self._prepare_inputs_for_e2e_qg(context)
+
+        # TODO: when overrding default_generate_kwargs all other arguments need to be passsed
+        # find a better way to do this
+        if not generate_kwargs:
+            generate_kwargs = self.default_generate_kwargs
+        
+        input_length = inputs["input_ids"].shape[-1]
+        
+        # max_length = generate_kwargs.get("max_length", 256)
+        # if input_length < max_length:
+        #     logger.warning(
+        #         "Your max_length is set to {}, but you input_length is only {}. You might consider decreasing max_length manually, e.g. summarizer('...', max_length=50)".format(
+        #             max_length, input_length
+        #         )
+        #     )
+
+        outs = self.model.generate(
+            input_ids=inputs['input_ids'].to(self.device), 
+            attention_mask=inputs['attention_mask'].to(self.device),
+            **generate_kwargs
+        )
+
+        prediction = self.tokenizer.decode(outs[0], skip_special_tokens=True)
+        questions = prediction.split("<sep>")
+        questions = [question.strip() for question in questions[:-1]]
+        return questions
+    
+    def _prepare_inputs_for_e2e_qg(self, context):
+        source_text = f"generate questions: {context}"
+        if self.model_type == "t5":
+            source_text = source_text + " </s>"
+        
+        inputs = self._tokenize([source_text], padding=False)
+        return inputs
+    
+    def _tokenize(
+        self,
+        inputs,
+        padding=True,
+        truncation=True,
+        add_special_tokens=True,
+        max_length=512
+    ):
+        inputs = self.tokenizer.batch_encode_plus(
+            inputs, 
+            max_length=max_length,
+            add_special_tokens=add_special_tokens,
+            truncation=truncation,
+            padding="max_length" if padding else False,
+            pad_to_max_length=padding,
+            return_tensors="pt"
+        )
+        return inputs
+
+
+SUPPORTED_TASKS = {
+    "e2e-qg": {
+        "impl": E2EQGPipeline,
+        "default": {
+            "model": "valhalla/t5-small-e2e-qg",
+        }
+    }
+}
+
+def pipeline(
+    task: str,
+    model: Optional = None,
+    tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+    qg_format: Optional[str] = "highlight",
+    ans_model: Optional = None,
+    ans_tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+    use_cuda: Optional[bool] = True,
+    **kwargs,
+):
+    # Retrieve the task
+    if task not in SUPPORTED_TASKS:
+        raise KeyError("Unknown task {}, available tasks are {}".format(task, list(SUPPORTED_TASKS.keys())))
+
+    targeted_task = SUPPORTED_TASKS[task]
+    task_class = targeted_task["impl"]
+
+    # Use default model/config/tokenizer for the task if no model is provided
+    if model is None:
+        model = targeted_task["default"]["model"] #
+    
+    # Try to infer tokenizer from model or config name (if provided as str)
+    if tokenizer is None:
+        if isinstance(model, str):
+            tokenizer = model
+        else:
+            # Impossible to guest what is the right tokenizer here
+            raise Exception(
+                "Impossible to guess which tokenizer to use. "
+                "Please provided a PretrainedTokenizer class or a path/identifier to a pretrained tokenizer."
+            )
+    
+    # Instantiate tokenizer if needed
+    if isinstance(tokenizer, (str, tuple)):
+        if isinstance(tokenizer, tuple):
+            # For tuple we have (tokenizer name, {kwargs})
+            tokenizer = AutoTokenizer.from_pretrained(tokenizer[0], **tokenizer[1])
+        else:
+            tokenizer = AutoTokenizer.from_pretrained(tokenizer)
+    
+    # Instantiate model if needed
+    if isinstance(model, str):
+        model = AutoModelForSeq2SeqLM.from_pretrained(model)
+    
+    if task == "question-generation":
+        if ans_model is None:
+            # load default ans model
+            ans_model = targeted_task["default"]["ans_model"]
+            ans_tokenizer = AutoTokenizer.from_pretrained(ans_model)
+            ans_model = AutoModelForSeq2SeqLM.from_pretrained(ans_model)
+        else:
+            # Try to infer tokenizer from model or config name (if provided as str)
+            if ans_tokenizer is None:
+                if isinstance(ans_model, str):
+                    ans_tokenizer = ans_model
+                else:
+                    # Impossible to guest what is the right tokenizer here
+                    raise Exception(
+                        "Impossible to guess which tokenizer to use. "
+                        "Please provided a PretrainedTokenizer class or a path/identifier to a pretrained tokenizer."
+                    )
+            
+            # Instantiate tokenizer if needed
+            if isinstance(ans_tokenizer, (str, tuple)):
+                if isinstance(ans_tokenizer, tuple):
+                    # For tuple we have (tokenizer name, {kwargs})
+                    ans_tokenizer = AutoTokenizer.from_pretrained(ans_tokenizer[0], **ans_tokenizer[1])
+                else:
+                    ans_tokenizer = AutoTokenizer.from_pretrained(ans_tokenizer)
+
+            if isinstance(ans_model, str):
+                ans_model = AutoModelForSeq2SeqLM.from_pretrained(ans_model)
+    
+    if task == "e2e-qg":
+        return task_class(model=model, tokenizer=tokenizer, use_cuda=use_cuda)
+    elif task == "question-generation":
+        return task_class(model=model, tokenizer=tokenizer, ans_model=ans_model, ans_tokenizer=ans_tokenizer, qg_format=qg_format, use_cuda=use_cuda)
+    else:
+        return task_class(model=model, tokenizer=tokenizer, ans_model=model, ans_tokenizer=tokenizer, qg_format=qg_format, use_cuda=use_cuda)
